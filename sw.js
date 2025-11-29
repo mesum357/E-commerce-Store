aaf13390eedad70d5dcdcec723c72fa3ad4c6ca2
Mesum Abbas <i.mesumabbas#@gmail.com>
2025-11-30 01:31:56 +0500

    First Commit

--- sw.js
+++ sw.js
@@ -3,15 +3,43 @@ const CACHE_NAME = 'discipline-schedule-v1';
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(['/', '/index.html']);
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/icon-192.svg',
+                '/icon-512.svg'
+            ]).catch((err) => {
+                console.log('Cache addAll failed:', err);
+            });
         })
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).catch(() => {
+                if (event.request.destination === 'document') {
+                    return caches.match('/index.html');
+                }
+            });
         })
     );
 });
